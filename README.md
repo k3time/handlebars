a038677cdbf16278c6350070b97bb05f0acd0a9b
Kuldeep Kaushik <kuldeepk@damcogroup.com>
2016-11-07 19:18:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# handlebars template engine with bootstrap
+a node.js application using express.js, handlebars template engine and bootstrap
+it explains the following
+   Middleware- static
+   Middleware- net server
+   Middleware- body-parser
+   Middleware- express-session
+   Middleware- express-handlebars
+   Middleware- bootstrap
---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express'); // Third party module
+var app = express(); // Initailising express or creating instance of it
+
+var bodyparser = require('body-parser'); // Third party module
+app.use(bodyparser.json()); // Following two lines needed in case we 
+app.use(bodyparser.urlencoded({extended:false})); // using body parser
+
+var routes = require('./routes/route'); // calling route.js
+
+var session = require('express-session'); // Third party module
+app.use(session({secret: "secret",  resave : true,  saveUninitialized : false}));
+
+app.use(express.static(__dirname + "/")); // to set folder to read by browser
+
+var expressHbs = require('express-handlebars'); // Third party module
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'layout.hbs'}));
+app.set('view engine', 'hbs');
+
+/*
+//Following code to create a server
+
+var net = require('net'); //core module
+var netServer = net.createServer();// To create TCP/IP server*/
+
+app.get('/', routes.defaultHandler);
+app.get('/login', routes.loginHandler);
+app.get('/logout', routes.logoutHandler);
+app.get('/loginSuccess', routes.loginSuccessHandler);
+app.post('/city', routes.cityHandler);
+app.get('/cancel', function(req, res){
+	res.send('Cancel');
+});
+app.get('/student', routes.studentHandler);
+app.get('/player/:name/:country', routes.playerHandler);
+
+app.use("*", function(req, res) {
+     res.status(404);
+     res.render('404.hbs', {});
+});
+
+app.use(function(error, req, res, next) {
+     console.log('Error : 500::' + error);
+     res.status(500);
+     res.render('500.hbs', {err:error});  // good for knowledge but don't do it
+});
+
+
+var port = process.env.PORT || 3333;
+app.listen(port, function(){ //To run local server and show command line message
+	console.log('HTTP server is listening on port: ' + port);
+}); 
